c2087388f3527d5f2e192debb932a9d342a24c2a
luxianping <654894017@qq.com>
2024-03-03 01:41:32 +0800

    1.移除无用代码

--- src/main/java/com/damon/tcc/TccSubNestService.java
+++ src/main/java/com/damon/tcc/TccSubNestService.java
@@ -14,6 +14,7 @@ import com.damon.tcc.sub_log.ITccSubLogService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.function.BiFunction;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
@@ -42,7 +43,7 @@ public abstract class TccSubNestService<R, P extends SubBizId> {
      * @param <PD>
      * @return
      */
-    protected <PD> R attempt(P parameter, Function<P, PD> attemptFunction, TwoParameterFunction<P, PD, R> executeLocalTransactionFunction) throws TccTryException {
+    protected <PD> R attempt(P parameter, Function<P, PD> attemptFunction, BiFunction<P, PD, R> executeLocalTransactionFunction) throws TccTryException {
         try {
             PD pd = attemptFunction.apply(parameter);
             R result = localTransactionService.execute(() ->
--- src/main/java/com/damon/tcc/function/TwoParameterFunction.java
+++
@@ -1,6 +0,0 @@
-package com.damon.tcc.function;
-
-@FunctionalInterface
-public interface TwoParameterFunction<P1, P2, R> {
-    R apply(P1 p1, P2 p2);
-}
